1590f2b99ca97ab8cc29b9586ebbc853396235c6
Andrey Temchenko <temchenko32@gmail.com>
2017-03-10 07:40:22 +0200

    change production build folder from ‘dist’ to ‘build’

--- vbuild.config.js
+++ vbuild.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 // this will copy ./static/** to ./dist/**
 module.exports = options => ({
   entry: 'src/index.js',
+  dist: 'build',
   proxy: 'http://localhost:5000/api',
   postcss: [
     // add more postcss plugins here

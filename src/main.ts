5f84482e77432d122c78005d07ae089dfe8e6a70
Andrey Temchenko <temchenko32@gmail.com>
2017-03-13 07:55:18 +0200

    fixes path to toastr css

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import '../node_modules/ng2-toastr/bundles/ng2-toastr.min.css';
-
 if (environment.production) {
   enableProdMode();
 }

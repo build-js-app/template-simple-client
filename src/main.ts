e99729ad6a5dd019641550240a3c754713e5dd7c
Andrey Temchenko <temchenko32@gmail.com>
2017-03-13 07:39:50 +0200

    bug fixes: toastr use

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import '../node_modules/ng2-toastr/bundles/ng2-toastr.min.css';
+
 if (environment.production) {
   enableProdMode();
 }
